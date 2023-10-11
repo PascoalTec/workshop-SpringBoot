daa9f870a6dfdd112e5d1c402d4469a9988d3ff7
Mormysh <leonardo.paascoal@gmail.com>
2023-10-11 16:44:11 -0300

    Services

--- src/main/java/com/cursoSpringBoot/course/Repository/UserRepository.java
+++ src/main/java/com/cursoSpringBoot/course/Repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.cursoSpringBoot.course.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.cursoSpringBoot.course.Entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
